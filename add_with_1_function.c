35add480f1601b6e28fbf9796ca24161ea7803a1
Nayanapriya <75724193+Nayanapriya@users.noreply.github.com>
2021-02-01 16:48:05 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,10 +2,16 @@
 #include<stdio.h>
 int main()
 {
-    int n1,n2,sum;
+    int n1,n2,b;
     printf("enter two numbers");
     scanf("%d%d",&n1,&n2);
-    sum=n1+n2;
-    printf("sum=%d",sum);
+    b=sum(n1,n2);
+    printf("sum=%d",b);
     return (0);
+}
+int sum(int n1,int n2)
+{
+    int n3;
+    n3=n1+n2;
+    return(n3);
 }
