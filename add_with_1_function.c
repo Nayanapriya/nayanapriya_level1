947cdee49ff83002d95fb11bf0f24172195ff4c2
Nayanapriya <75724193+Nayanapriya@users.noreply.github.com>
2021-02-08 10:13:14 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -6,7 +6,8 @@ int main()
 	printf("enter 2 numbers");
 	scanf("%d%d,&n1,&n2");
 	sum=n1+n2;
-	printf("sumof %d and %d is %d,n1,n2,sum);
+	printf("sumof %d and %d is %d",n1,n2,sum);
+	return sum;
  }
 	
 
