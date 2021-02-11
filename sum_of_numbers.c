cc51acbbb64b22eabed110e3cebd24cc5b9f7556
Nayanapriya <75724193+Nayanapriya@users.noreply.github.com>
2021-02-11 13:03:08 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -13,17 +13,17 @@ void input_array(int n, int a[n])
 {
     printf("Enter elements in array : ");
     for( int i=0; i<n; i++)
-    {
+    
         scanf("%d",&a[i]);
-    }
+    
 }
 
 int compute(int n, int a[n])
 {
     int sum=0;
-    for(int i=0;i<n;i++) {
+    for(int i=0;i<n;i++) 
         sum += a[i];
-    }
+    
     return sum;
 }
 
