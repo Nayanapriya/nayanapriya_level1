692bc05fab328d3f5ada8b048ce0624ee6cd2b82
Nayanapriya <75724193+Nayanapriya@users.noreply.github.com>
2021-02-18 10:07:52 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -17,19 +17,22 @@ float compute(fraction f1,fraction f2)
     sum=(((f1.num*f2.den)+(f2.num*f1.den))/(f1.den*f2.den));
     return sum;
 }
-void output(fraction f1,fraction f2,float sum)
+void output(fraction f1,fraction f2,float sum,float numerator,float denominator)
 {
-    printf("The addition of %f/%f and %f/%f is %f",f1.num,f1.den,f2.num,f2.den,sum);
+    printf("The addition of %f/%f and %f/%f in float form is %f and in fraction form is %f/%f",f1.num,f1.den,f2.num,f2.den,sum,numerator,denominator);
     
 }
 int main()
 {
     fraction f1,f2;
     float sum;
+    float numerator,denominator;
     f1=input();
     f2=input();
     sum=compute(f1,f2);
-    output(f1,f2,sum);
+    numerator=(f1.num*f2.den)+(f2.num*f1.den);
+    denominator=(f1.den*f2.den);
+    output(f1,f2,sum,numerator,denominator);
     return 0;
     
 }
