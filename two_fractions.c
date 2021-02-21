d3ea2d2ebdd6c080a96a75ac6f927b6527a5bc07
Nayanapriya <75724193+Nayanapriya@users.noreply.github.com>
2021-02-21 17:57:39 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,40 +2,51 @@
 #include <stdio.h>
 typedef struct fraction
 {
-    float num,den;
+    int num,den;
 }fraction;
 fraction input()
 {
     fraction f;
     printf("enter the numerator and denominator of fraction 1 and fraction 2 respectively");
-    scanf("%f%f",&f.num,&f.den);
+    scanf("%d%d",&f.num,&f.den);
     return f;
 }
-float compute(fraction f1,fraction f2)
+int gcd(int x,int y)
 {
-    float sum;
-    int numerator,denominator;
-    sum=(((f1.num*f2.den)+(f2.num*f1.den))/(f1.den*f2.den));
-    numerator=((f1.num*f2.den)+(f2.num*f1.den));
-    denominator=(f1.den*f2.den);
-    printf("The addition of %f/%f and %f/%f in fraction form is %d/%d ",f1.num,f1.den,f2.num,f2.den,numerator,denominator);
-    return sum;
+    while(x!=y)
+    {
+        if(x>y)
+            x=x-y;
+        else
+            y=y-x;
+        
+    }
+    return x;
 }
-void output(float sum)
+
+fraction compute(fraction f1,fraction f2)
 {
-    printf("and the simplified form is %f",sum);
+    fraction f3, sum;
+    f3.den=gcd(f1.den,f2.den);
+    f3.den=(f1.den*f2.den)/f3.den;
+    f3.num=(f1.num)*(f3.den/f1.den)+(f2.num)*(f3.den/f2.den);
+    int c_f=gcd(f3.num,f3.den);
+    f3.den=f3.den/c_f;
+    f3.num=f3.num/c_f;
+    return f3;
+}
+void output(fraction f1,fraction f2,fraction f3)
+{
+    printf("The addition of %d/%d and %d/%d in fraction form is %d/%d ",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
     
 }
 int main()
 {
-    fraction f1,f2;
-    float sum;
-    float numerator,denominator;
+    fraction f1,f2,f3;
     f1=input();
     f2=input();
-    sum=compute(f1,f2);
-    
-    output(sum);
+    f3=compute(f1,f2);
+    output(f1,f2,f3);
     return 0;
     
 }
